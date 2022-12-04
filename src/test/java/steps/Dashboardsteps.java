6996496203e62471e1313a0b02e05de3c8bd2255
serjik900 <serhiimedia@gmail.com>
2022-12-04 12:03:09 -0600

    Initial commit

---
+++ src/test/java/steps/Dashboardsteps.java
@@ -0,0 +1,10 @@
+package steps;
+
+import io.cucumber.java.en.Then;
+
+public class Dashboardsteps {
+    @Then("user verify dashboard page")
+public void user_verify_dashboard_page() {
+        System.out.println("Batch 14 is happy now");
+}
+}
